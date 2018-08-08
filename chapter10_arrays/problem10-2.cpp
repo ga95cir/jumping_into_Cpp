27ce11366c32d4c035bff0c39b1b0653c4a5aae1
casp_has <caspar.hasner@tum.de>
2018-08-08 22:05:56 +0200

    implemented problem 10-2, review method enter_array to account for non-integer inputs

--- chapter10_arrays/problem10-2.cpp
+++ chapter10_arrays/problem10-2.cpp
@@ -1,8 +1,91 @@
+/*
+Problem 10-2: Write a program that takes in 50 values
+and prints out the highest, the lowest, the average and
+then all 50 input values, one per line.
+*/
+
 #include <iostream>
 using namespace std;
 
-int main() 
+void enter_array(int arr[], int size);
+int find_max(int arr[], int size);
+int find_min(int arr[], int size);
+float find_average(int arr[], int size);
+void display_array(int arr[], int size);
+
+
+int main()
 {
-    cout << "Hello, World!";
-    return 0;
+	int size = 50;
+	int arr[size];
+	enter_array(arr, size);
+	int max = find_max(arr, size);
+	int min = find_min(arr, size);
+	float average = find_average(arr, size);
+
+	cout << "Maximum: " << max << endl;
+	cout << "Minimum: " << min << endl;
+	cout << "Average: " << average << endl;
+
+	display_array(arr, size);
+
+	return 0;
+}
+
+
+
+
+void enter_array(int arr[], int size)
+{
+	for (int i=0; i<size; i++)
+	{
+		cout << "Enter value " << i << ": ";
+		cin >> arr[i];
+	}
+}
+
+int find_max(int arr[], int size)
+{
+	int max = arr[0];
+	for (int i=1; i<size; i++)
+	{
+		if (max<arr[i])
+		{
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+int find_min(int arr[], int size)
+{
+	int min = arr[0];
+	for (int i=1; i<size; i++)
+	{
+		if (min>arr[i])
+		{
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+float find_average(int arr[], int size)
+{
+	float sum = arr[0];
+	float average;
+	for (int i=1; i<size; i++)
+	{
+		sum += arr[i];
+	}
+	average = sum / size;
+	return average;
+}
+
+void display_array(int arr[], int size)
+{
+	for (int i=0; i<size; i++)
+	{
+		cout << "Index " << i << ": " << arr[i] << endl;
+	}
 }
