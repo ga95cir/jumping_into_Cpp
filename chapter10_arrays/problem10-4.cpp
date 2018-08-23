08d1f01a9fbdda51e3dd590581b10e0ede0b5bf6
casp_has <caspar.hasner@tum.de>
2018-08-23 21:35:38 +0200

    update

--- chapter10_arrays/problem10-4.cpp
+++ chapter10_arrays/problem10-4.cpp
@@ -11,10 +11,10 @@ won by either side before the entire grid is filled?
 #include <cstdlib>
 using namespace std;
 
-int init_board();
+void init_board(int board[][3]);
 void display_board(int board[][3]);
-void make_move(int board[][3]; int player);
-void check_win(int board[][3]);
+void make_move(int board[][3], int player);
+bool check_win(int board[][3], bool game);
 int change_player(int player);
 
 
@@ -23,29 +23,29 @@ At start of game, maybe write a text to explain the rules.
 */
 int main()
 {
-    int board[3][3] = init_board();
+    int board[3][3];
+    init_board(board);
     int player = 1;
-    while (1)
+    bool game = true;
+    while (game)
     {
         display_board(board);
         make_move(board, player);
-        check_win(board);
-        player = change_player();
+        game = check_win(board, game);
+        player = change_player(player);
     }
     return 0;
 }
 
-int init_board()
+void init_board(int board[][3])
 {
-    int arr[3][3];
     for (int i=0; i<3; i++)
     {
         for (int j=0; j<3; j++)
         {
-            arr[i][j] = 0;
+            board[i][j] = 0;
         }
     }
-    return arr;
 }
 
 void display_board(int board[3][3])
@@ -60,7 +60,95 @@ void display_board(int board[3][3])
 
 void make_move(int board[][3], int player)
 {
+    bool bo = true;
     int row = 0;
     int col = 0;
+    while (bo)
+    {
+        cout << "Player " << player << " please pick row: " << endl;
+        cin >> row;
+        cout << "and column: ";
+        cin >> col;
 
+        if (board[row][col]==1 || board[row][col]==2)
+        {
+            cout << "Invalid move! Please try again!" << endl;
+        }
+        else
+        {
+            board[row][col] = player;
+            bo = false;
+        }
+    }
+}
+
+bool check_win(int board[][3], bool game)
+{
+    int win;
+    int tie;
+
+    win = board[0][0] + board[0][1] + board[0][2];
+    if (win==3)
+    {
+        cout << "Player 1 wins!" << endl;
+        game = false;
+    }
+    if (win==6)
+    {
+        cout << "Player 2 wins!" << endl;
+        game = false;
+    }
+    win = board[1][0] + board[1][1] + board[1][2];
+    if (win==3)
+    {
+        cout << "Player 1 wins!" << endl;
+        game = false;
+    }
+    if (win==6)
+    {
+        cout << "Player 2 wins!" << endl;
+        game = false;
+    }
+    win = board[2][0] + board[2][1] + board[2][2];
+    if (win==3)
+    {
+        cout << "Player 1 wins!" << endl;
+        game = false;
+    }
+    if (win==6)
+    {
+        cout << "Player 2 wins!" << endl;
+        game = false;
+    }
+
+    for (int i=0; i<3; i++)
+    {
+        for (int j=0; j<3; j++)
+        {
+            if (board[i][j]==1 || board[i][j]==2)
+            {
+                tie += 1;
+            }
+        }
+    }
+
+    if (tie==9)
+    {
+        cout << "It's a tie!" << endl;
+        game = false;
+    }
+
+    return game;
+}
+
+int change_player(int player)
+{
+    if (player==1)
+    {
+        return 2;
+    }
+    else
+    {
+        return 1;
+    }
 }
